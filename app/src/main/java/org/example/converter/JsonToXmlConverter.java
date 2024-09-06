0076bf4f2778c7a9e9a3ecdd28d4930a6e49e313
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-09-06 11:50:41 +0300

    Добавил скобки при единичных выражениях

--- app/src/main/java/org/example/converter/JsonToXmlConverter.java
+++ app/src/main/java/org/example/converter/JsonToXmlConverter.java
@@ -9,13 +9,15 @@ public class JsonToXmlConverter {
     public static void convertCityFromJsonToXml(String jsonFilePath) {
         City city = JsonParser.parseCity(jsonFilePath);
 
-        if (city == null)
+        if (city == null) {
             return;
+        }
 
         String xml = XmlConverter.toXML(city);
 
-        if (xml == null)
+        if (xml == null) {
             return;
+        }
 
         String xmlFilePath = jsonFilePath.replace(".json", ".xml");
         CustomFileWriter.writeToFile(xml, xmlFilePath);
