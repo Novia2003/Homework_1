f7f2f4e3c0e01e1decc57d32c5bee39b6402dc14
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-09-08 15:23:23 +0300

    Сделала названия тестов более подробными

--- app/src/test/java/org/example/customLinkedList/CustomLinkedListTest.java
+++ app/src/test/java/org/example/customLinkedList/CustomLinkedListTest.java
@@ -1,9 +1,11 @@
 package org.example.customLinkedList;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,15 +20,23 @@ class CustomLinkedListTest {
     }
 
     @Test
-    public void testAdd() {
+    @DisplayName("Test adding elements to the list")
+    public void testAddElementsToList() {
         assertTrue(list.add(1));
+        assertTrue(list.contains(1));
+
         assertTrue(list.add(2));
+        assertTrue(list.contains(2));
+
         assertTrue(list.add(3));
+        assertTrue(list.contains(3));
+
         assertEquals(3, list.size());
     }
 
     @Test
-    public void testGetFirst() {
+    @DisplayName("Test getting the first element")
+    public void testGetFirstElementsFromList() {
         list.add(1);
         list.add(2);
 
@@ -34,12 +44,14 @@ class CustomLinkedListTest {
     }
 
     @Test
-    public void testGetFirstEmptyList() {
+    @DisplayName("Return NoSuchElementException when getting the first element from an empty list")
+    public void testGetFirstElementFromEmptyList() {
         assertThrows(NoSuchElementException.class, () -> list.getFirst());
     }
 
     @Test
-    public void testGet() {
+    @DisplayName("Test getting elements by index")
+    public void testGetElementByIndex() {
         list.add(1);
         list.add(2);
         list.add(3);
@@ -50,7 +62,8 @@ class CustomLinkedListTest {
     }
 
     @Test
-    public void testGetOutOfBounds() {
+    @DisplayName("Return IndexOutOfBoundsException when getting elements by out-of-bounds index")
+    public void testGetElementsByOutOfBoundsIndex() {
         list.add(1);
 
         assertThrows(IndexOutOfBoundsException.class, () -> list.get(1));
@@ -58,7 +71,8 @@ class CustomLinkedListTest {
     }
 
     @Test
-    public void testRemove() {
+    @DisplayName("Test removing the first element")
+    public void testRemoveFirstElementFromList() {
         list.add(1);
         list.add(2);
         list.add(3);
@@ -69,12 +83,14 @@ class CustomLinkedListTest {
     }
 
     @Test
-    public void testRemoveEmptyList() {
+    @DisplayName("Return NoSuchElementException when removing the first element from an empty list")
+    public void testRemoveFirstElementFromEmptyList() {
         assertThrows(NoSuchElementException.class, () -> list.remove());
     }
 
     @Test
-    public void testRemoveAtIndex() {
+    @DisplayName("Test removing an element by index")
+    public void testRemoveElementByIndex() {
         list.add(1);
         list.add(2);
         list.add(3);
@@ -86,7 +102,8 @@ class CustomLinkedListTest {
     }
 
     @Test
-    public void testRemoveAtIndexOutOfBounds() {
+    @DisplayName("Return IndexOutOfBoundsException when removing an element by out-of-bounds index")
+    public void testRemoveElementByOutOfBoundsIndex() {
         list.add(1);
 
         assertThrows(IndexOutOfBoundsException.class, () -> list.remove(1));
@@ -94,7 +111,8 @@ class CustomLinkedListTest {
     }
 
     @Test
-    public void testContains() {
+    @DisplayName("Test checking if the list contains an element")
+    public void testContainsElementInCustomLinkedList() {
         list.add(1);
         list.add(7);
         list.add(3);
@@ -106,23 +124,27 @@ class CustomLinkedListTest {
     }
 
     @Test
-    public void testAddAll() {
+    @DisplayName("Test adding all elements from a collection")
+    public void testAddAllElementsToCustomLinkedListFromCollection() {
         assertTrue(list.addAll(Arrays.asList(1, 2, 3)));
 
-        assertEquals(3, list.size());
         assertEquals(1, list.get(0));
         assertEquals(2, list.get(1));
         assertEquals(3, list.get(2));
+
+        assertEquals(3, list.size());
     }
 
     @Test
-    public void testAddAllEmptyCollection() {
-        assertFalse(list.addAll(Arrays.asList()));
+    @DisplayName("Return false when adding all elements from an empty collection")
+    public void testAddAllElementsToCustomLinkedListFromEmptyCollection() {
+        assertFalse(list.addAll(List.of()));
         assertEquals(0, list.size());
     }
 
     @Test
-    public void testAddAllCustomLinkedList() {
+    @DisplayName("Test adding all elements from another CustomLinkedList")
+    public void testAddAllElementToCustomLinkedListFromAnotherCustomLinkedList() {
         CustomLinkedList<Integer> otherList = new CustomLinkedList<>();
         otherList.add(4);
         otherList.add(5);
@@ -133,18 +155,21 @@ class CustomLinkedListTest {
         list.add(3);
 
         assertTrue(list.addAll(otherList));
-        assertEquals(6, list.size());
+
         assertEquals(1, list.get(0));
         assertEquals(2, list.get(1));
         assertEquals(3, list.get(2));
         assertEquals(4, list.get(3));
         assertEquals(5, list.get(4));
         assertEquals(6, list.get(5));
+
+        assertEquals(6, list.size());
         assertEquals(3, otherList.size());
     }
 
     @Test
-    public void testAddAllEmptyCustomLinkedList() {
+    @DisplayName("Return false when adding all elements from an empty CustomLinkedList")
+    public void testAddAllElementsToCustomLinkedListFromEmptyCustomLinkedList() {
         CustomLinkedList<Integer> otherList = new CustomLinkedList<>();
 
         assertFalse(list.addAll(otherList));
@@ -152,6 +177,7 @@ class CustomLinkedListTest {
     }
 
     @Test
+    @DisplayName("Test getting the size of the list")
     public void testSize() {
         assertEquals(0, list.size());
 
@@ -167,5 +193,4 @@ class CustomLinkedListTest {
         list.remove();
         assertEquals(0, list.size());
     }
-
 }
