72780cb85f17202f8fa78d57478423fda1d898f3
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-09-22 01:13:56 +0300

    added models

---
+++ spring-app/src/main/java/ru/tbank/model/Category.java
@@ -0,0 +1,9 @@
+package ru.tbank.model;
+
+import lombok.Data;
+
+@Data
+public class Category {
+    private String slug;
+    private String name;
+}
---
+++ spring-app/src/main/java/ru/tbank/model/Location.java
@@ -0,0 +1,9 @@
+package ru.tbank.model;
+
+import lombok.Data;
+
+@Data
+public class Location {
+    private String slug;
+    private String name;
+}
