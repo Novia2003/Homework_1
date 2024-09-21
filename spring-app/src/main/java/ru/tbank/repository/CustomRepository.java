19d6eae8a61bef95f44e3756d33ad734f5f2ebec
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-09-22 01:16:56 +0300

    added the data storage and its configuration

---
+++ spring-app/src/main/java/ru/tbank/repository/CustomRepository.java
@@ -0,0 +1,38 @@
+package ru.tbank.repository;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Component
+public class CustomRepository<T> {
+
+    private final ConcurrentHashMap<Long, T> storage = new ConcurrentHashMap<>();
+
+    private final AtomicLong idCounter = new AtomicLong(1);
+
+    public Long save(T model) {
+        Long id = idCounter.getAndIncrement();
+        storage.put(id, model);
+
+        return id;
+    }
+
+    public T findById(Long id) {
+        return storage.get(id);
+    }
+
+    public Collection<T> findAll() {
+        return storage.values();
+    }
+
+    public void update(Long id, T model) {
+        storage.put(id, model);
+    }
+
+    public void delete(Long id) {
+        storage.remove(id);
+    }
+}
