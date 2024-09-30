6384f96935ef06f00d51111499d227521065560f
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-10-01 00:24:59 +0300

    connected the necessary dependencies jacoco, testcontainer and wiremock

--- spring-app/build.gradle.kts
+++ spring-app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
+    id("jacoco")
 }
 
 group = "ru.tbank"
@@ -11,6 +12,8 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 repositories {
     mavenCentral()
 }
+
+val wiremockTestcontainersVersion: String by extra("1.0-alpha-13")
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
@@ -23,8 +26,41 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testImplementation("io.rest-assured:rest-assured")
+    testImplementation("org.wiremock:wiremock-standalone:3.6.0")
+    testImplementation("org.wiremock.integrations.testcontainers:wiremock-testcontainers-module:$wiremockTestcontainersVersion")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.7")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+jacoco {
+    toolVersion = "0.8.8"
+}
+
+val jacocoExclusions = listOf(
+    "ru/tbank/dto/**",
+    "ru/tbank/model/**",
+    "ru/tbank/configuration/**"
+)
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    classDirectories.setFrom(
+        files(
+            classDirectories.files.map {
+                fileTree(it) {
+                    exclude(jacocoExclusions)
+                }
+            }
+        )
+    )
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+        html.required.set(true)
+    }
 }
