4accf0b12f5a3de56dad134717a590bf099726a9
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-10-08 19:38:00 +0300

    added possible error codes to swagger

--- spring-app-currency/src/main/java/ru/tbank/controller/CurrencyController.java
+++ spring-app-currency/src/main/java/ru/tbank/controller/CurrencyController.java
@@ -1,24 +1,25 @@
 package ru.tbank.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.tbank.dto.currency.CurrencyConvertRequestDTO;
 import ru.tbank.dto.currency.CurrencyConvertResponseDTO;
 import ru.tbank.dto.currency.CurrencyRateResponseDTO;
+import ru.tbank.dto.http.ErrorMessageResponse;
 import ru.tbank.service.CurrencyService;
 
 @RestController
-@RequestMapping("/currency")
 @Validated
+@RequestMapping("/currency")
 @RequiredArgsConstructor
 @Tag(
         name = "CurrencyController",
@@ -29,9 +30,47 @@ public class CurrencyController {
     private final CurrencyService currencyService;
 
     @GetMapping("rates/{code}")
-    @Operation(description = "Getting the currency exchange rate by its code")
+    @Operation(
+            description = "Getting the currency exchange rate by its code"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful operation",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = CurrencyRateResponseDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid input",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessageResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Currency rate not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessageResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "503",
+                            description = "Service unavailable",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessageResponse.class)
+                            )
+                    )
+            }
+    )
     public CurrencyRateResponseDTO getCurrencyRateByRate(
-            @NotBlank(message = "the \"code\" variable could not be blank")
+            @NotBlank(message = "the code variable could not be blank")
             @PathVariable
             String code
     ) {
@@ -39,8 +78,47 @@ public class CurrencyController {
     }
 
     @PostMapping("/convert")
-    @Operation(description = "Getting a list of user recipes")
+    @Operation(
+            description = "Getting a list of user recipes"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful operation",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = CurrencyConvertResponseDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid input",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessageResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Currency rate not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessageResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "503",
+                            description = "Service unavailable",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessageResponse.class)
+                            )
+                    )
+            }
+    )
     public CurrencyConvertResponseDTO convertCurrency(
+            @Valid
             @RequestBody
             CurrencyConvertRequestDTO dto) {
         return currencyService.convertCurrency(dto);
