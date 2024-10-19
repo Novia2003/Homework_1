c6d324413ff1782e694261543296aacfff67ba9d
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-10-20 01:06:48 +0300

    added the use of ExecutorService and Future during initialization

--- spring-app/src/main/java/ru/tbank/initializer/DataInitializer.java
+++ spring-app/src/main/java/ru/tbank/initializer/DataInitializer.java
@@ -2,8 +2,9 @@ package ru.tbank.initializer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.tbank.dto.category.CategoryKudagoResponseDTO;
 import ru.tbank.dto.location.LocationKudagoResponseDTO;
@@ -11,6 +12,10 @@ import ru.tbank.service.CategoryService;
 import ru.tbank.service.KudagoService;
 import ru.tbank.service.LocationService;
 import ru.tbank.timed.Timed;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
@@ -21,33 +26,49 @@ public class DataInitializer {
     private final LocationService locationService;
     private final KudagoService kudagoService;
 
-    @EventListener(ApplicationReadyEvent.class)
+    private final ExecutorService fixedThreadPool;
+
+    private final ScheduledExecutorService scheduledThreadPool;
+
+    @EventListener(ApplicationStartedEvent.class)
     @Timed
     public void init() {
         log.info("Starting data initialization...");
 
-        var categories = kudagoService.getCategories();
-        if (categories == null) {
-            log.info("No categories fetched. Received null response.");
-        } else {
-            log.info("Fetched {} categories.", categories.length);
+        scheduledThreadPool.schedule(this::initializeData, 0, TimeUnit.SECONDS);
+    }
 
-            for (CategoryKudagoResponseDTO category : categories) {
-                categoryService.createCategory(category);
+    @Scheduled(fixedDelayString = "${initialization.interval}")
+    @Timed
+    public void initializeData() {
+        log.info("Scheduled data initialization started...");
+
+        fixedThreadPool.submit(() -> {
+            var categories = kudagoService.getCategories();
+            if (categories == null) {
+                log.info("No categories fetched. Received null response.");
+            } else {
+                log.info("Fetched {} categories.", categories.length);
+
+                for (CategoryKudagoResponseDTO category : categories) {
+                    categoryService.createCategory(category);
+                }
             }
-        }
+        });
 
-        var locations = kudagoService.getLocations();
-        if (locations == null) {
-            log.info("No locations fetched. Received null response.");
-        } else {
-            log.info("Fetched {} locations.", locations.length);
+        fixedThreadPool.submit(() -> {
+            var locations = kudagoService.getLocations();
+            if (locations == null) {
+                log.info("No locations fetched. Received null response.");
+            } else {
+                log.info("Fetched {} locations.", locations.length);
 
-            for (LocationKudagoResponseDTO location : locations) {
-                locationService.createLocation(location);
+                for (LocationKudagoResponseDTO location : locations) {
+                    locationService.createLocation(location);
+                }
             }
-        }
+        });
 
-        log.info("Data initialization completed.");
+        log.info("Scheduled data initialization completed.");
     }
 }
