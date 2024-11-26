e54fd24558b7fe6a0753876b414058d1511627e7
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-11-27 00:27:05 +0300

    added logstash dependency

--- spring-app/build.gradle.kts
+++ spring-app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+    implementation("net.logstash.logback:logstash-logback-encoder:8.0")
 }
 
 tasks.test {
