4886f15290d937fc238037f2833f055320623fb7
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-11-27 22:37:31 +0300

    added a sample code to get OutOfMemory

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.example.customLinkedList.CustomLinkedListDemo;
 
+import static org.example.memory.OutOfMemoryExample.triggerOutOfMemory;
+
 public class App {
 
     public String getGreeting() {
@@ -9,7 +11,8 @@ public class App {
     }
 
     public static void main(String[] args) {
-        CustomLinkedListDemo.demonstrateCustomLinkedList();
+        //CustomLinkedListDemo.demonstrateCustomLinkedList();
+        triggerOutOfMemory();
     }
 
 }
